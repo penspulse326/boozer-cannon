30d2b6e8ea9443de420f026076114da798d46c8b
Shin <penspulse@gmail.com>
2026-09-17 23:10:24 +0800

    chore(web): remove @popperjs/core and unused bootstrap js plugin

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   css: ['~/assets/scss/main.scss'],
   devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['color-functions', 'import', 'global-builtin'],
+        },
+      },
+    },
+  },
 });
